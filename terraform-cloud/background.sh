0fb38b7bb2007442c8a0e5e2565934859766697b
Bill Wang <bill.wang>
2020-03-23 11:03:21 +1100

    add backgroup script to install terraform

---
+++ terraform-cloud/background.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bashs 
+
+apt update
+apt install -y curl unzip
+
+repo="hashicorp/terraform"
+latest=$(curl -s https://github.com/${repo}/releases/latest)
+latest=$(echo $latest\" |grep -oP '(?<=tag\/v)[0-9][^"]*')
+echo $latest
+
+curl -L https://releases.hashicorp.com/terraform/${latest}/terraform_${latest}_linux_amd64.zip -o terraform.zip
+unzip terraform.zip
+mv terraform /usr/bin/
+chmod +x /usr/bin/terraform
+rm -f terraform.zip
+terraform version
